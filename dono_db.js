42076f2d607bbdc8ca1617af7826fc879ebdcfcb
Bryan <81637460+whitechineseboi@users.noreply.github.com>
2026-05-10 02:47:51 +0800

    Add account creation function

--- dono_db.js
+++ dono_db.js
@@ -3,8 +3,8 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-//This is the SQL connector, create all your functions that call dono_DB here
-//Enivronment variables, my sql server is not the same your sql server
+//Enivronment variables to automatically log you into your sql server
+//.env file required, refer to README.txt
 const pool = mysql.createPool({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
@@ -12,21 +12,39 @@ const pool = mysql.createPool({
     database: process.env.MYSQL_DATABASE
 }).promise()
 
+
+//Function to call all accounts for testing purposes
 export async function getAccounts() {
-    const [rows] = await pool.query("SELECT * FROM accounts");
+    const [rows] = await pool.query("select * from dono_accounts");
     return rows;
 }
 
-export async function getAccount(id)
-{
-    const [rows] = await pool.query
-    ("select * from accounts where id = ?", [id])
-    return rows[0]
+
+//Function to insert new row through account creation page
+export async function createAccount(username, password, country) {
+
+    await pool.query(
+        "insert into dono_accounts (username, password, country) values (?, ?, ?)",
+        [username, password, country]
+    )
+
+    return {
+        username,
+        password,
+        country
+    }
 }
 
-export async function createAccount(name, id)
-{
-    const result = await pool.query
-    ("insert into accounts (name, id) values (?, ?)", [name, id])
-    return {name, id};
-}
+// export async function getAccount(id)
+// {
+//     const [rows] = await pool.query
+//     ("select * from accounts where id = ?", [id])
+//     return rows[0]
+// }
+
+// export async function createAccount(name, id)
+// {
+//     const result = await pool.query
+//     ("insert into accounts (name, id) values (?, ?)", [name, id])
+//     return {name, id};
+// }
